a1aec232fee30be501abd22f014ae2bd1ad600bd
SaifAmirr <saif.1000.ebrahim@gmail.com>
2025-01-04 15:26:27 +0200

    modified the dbt modelling dag

--- dags/modeling/dbt_modelling_dag.py
+++ dags/modeling/dbt_modelling_dag.py
@@ -15,8 +15,8 @@ with DAG(
     trigger_dbt_job = DbtCloudRunJobOperator(
         task_id='trigger_dbt_job',
         conn_id='dbt_cloud_default',  # Specify your Airflow connection ID
-        job_id=70471823413982,  # Replace with your dbt Cloud job ID
-        timeout=600,  # Timeout for the job execution (in seconds)
+        run_id=70471823413982,  # Replace with your dbt Cloud job ID
+        account_id = 70471823411227,
         poll_interval=30,  # Poll every 30 seconds for status updates
     )
     
